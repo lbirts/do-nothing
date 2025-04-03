5c29c25bbafdf444024228ca8d2f869347c56a8e
lbirts <birtslauren@gmail.com>
2025-04-03 10:21:29 -0700

    add low lights

--- src/app/start/[slug]/page.tsx
+++ src/app/start/[slug]/page.tsx
@@ -2,6 +2,9 @@
 
 import MovementAlert from "@/components/MovementAlert";
 import { Usable, use, useEffect, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
+import classNames from "classnames";
+import LowLightText from "@/components/LowLightText";
 
 type Param = {
   slug: string;
@@ -11,34 +14,62 @@ type Props = {
 };
 
 export default function Start({ params }: Props) {
+  const router = useRouter();
   const { slug } = use(params);
   const initialSecondsRef = useRef(Number(slug) * 60);
   const [timeRemaining, setTimeRemaining] = useState(initialSecondsRef.current);
+  const [lowLights, setLowLights] = useState(false);
 
   useEffect(() => {
     let interval: NodeJS.Timeout;
 
     if (timeRemaining > 0) {
       interval = setInterval(() => {
-        setTimeRemaining((prevTime) => prevTime - 1);
+        setTimeRemaining((prevTime) => {
+          const newTime = prevTime - 1;
+
+          if (initialSecondsRef.current - newTime === 6 && !lowLights) {
+            setLowLights(true);
+          }
+
+          return newTime;
+        });
       }, 1000);
     } else if (timeRemaining === 0) {
+      setTimeout(() => {
+        router.push("/");
+      }, 2000);
     }
 
     return () => {
       if (interval) clearInterval(interval);
     };
-  }, [timeRemaining]);
+  }, [timeRemaining, lowLights]);
 
   const resetTimer = () => {
     setTimeRemaining(initialSecondsRef.current);
-  }
+    setLowLights(false);
+  };
 
   return (
     <>
-      <h3 className="font-serif text-xl sm:text-2xl font-black text-neutral-400">
+      <LowLightText lowLights={lowLights} />
+      <h3
+        className={classNames(
+          "font-serif text-xl sm:text-2xl font-black text-neutral-400 transition-all duration-1000",
+          lowLights ? "opacity-50" : "opacity-100"
+        )}
+      >
         Please be idle for {timeRemaining} seconds.
       </h3>
+      <h2
+        className={classNames(
+          "font-serif text-lg sm:text-xl font-black text-neutral-600 transition-all duration-500",
+          timeRemaining === 0 ? "opacity-100" : "opacity-0"
+        )}
+      >
+        Redirecting you back to do something.
+      </h2>
       <MovementAlert action={resetTimer} />
     </>
   );
--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -1,30 +1,42 @@
 "use client";
 
+import LowLightText from "@/components/LowLightText";
 import MovementAlert from "@/components/MovementAlert";
 import { useEffect, useState } from "react";
+import classNames from "classnames";
 
 export default function Start() {
   const [timer, setTimer] = useState(0);
+  const [lowLights, setLowLights] = useState(false);
 
   useEffect(() => {
     let interval: NodeJS.Timeout;
 
     interval = setInterval(() => {
-        setTimer((prevTime) => prevTime + 1);
+      setTimer((prevTime) => {
+        const newTime = prevTime + 1;
+
+        if (newTime >= 7 && !lowLights) {
+          setLowLights(true);
+        }
+        return newTime;
+      });
     }, 1000);
 
     return () => {
       if (interval) clearInterval(interval);
     };
-  }, [timer]);
+  }, []);
 
   const resetTimer = () => {
     setTimer(0);
+    setLowLights(false);
   }
 
   return (
     <>
-      <h3 className="font-serif text-xl sm:text-2xl font-black text-neutral-400">
+      <LowLightText lowLights={lowLights} />
+      <h3 className={classNames("font-serif text-xl sm:text-2xl font-black text-neutral-400 transition-all duration-1000", lowLights ? "opacity-50" : "opacity-100")}>
         You've been idle for {timer} seconds.
       </h3>
       <MovementAlert action={resetTimer} />
